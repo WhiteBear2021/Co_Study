ab4d2a9f9c66a72a096708558f133e82ad6d2be4
김수현 <suhyunkimbb@naver.com>
2021-04-08 15:02:32 +0900

    commit signupformaction

--- CoStudy/src/CoStudy/action/user/SignUpFormAction.java
+++ CoStudy/src/CoStudy/action/user/SignUpFormAction.java
@@ -1,5 +1,19 @@
 package CoStudy.action.user;
 
-public class SignUpFormAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import CoStudy.action.Action;
+import CoStudy.action.ActionForward;
+
+public class SignUpFormAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ActionForward forward= new ActionForward();
+		forward.setRedirect(false);
+		forward.setPath("/signUpform.jsp");
+		return forward;
+	}
 
 }
