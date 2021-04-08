7014639b3835d4a6218cf047bdee5fffcc00f375
juwon an <juwoon01@naver.com>
2021-04-08 16:26:59 +0900

    add timer to groupMain.html

--- CoStudy/WebContent/js/timer.js
+++ CoStudy/WebContent/js/timer.js
@@ -1,15 +1,18 @@
 let isrunning = false;
 let time = 0;
+let hour = 0;
+let min = 0;
+let sec = 0;
 
 $(function(){
 
-    $('#bnt').click(function(){
+    $('#timer_bnt_start').click(function(){
         if(isrunning){//중지
             isrunning = false;
-
+            $(this).val("시작");
         }else{//시작
             isrunning = true;
-            $('#bnt').val = "중지";
+            $(this).val("중지");
             startTimer();
         }
     });
@@ -18,10 +21,20 @@ $(function(){
 function startTimer(){
     setTimeout(function tick(){
         if(isrunning){
-            console.log(time);
             time++;
-            setTimeout(tick(),1000);
+            output();
+            setTimeout(tick,1000);
         }
-
     },1000)
+}
+
+function output(){
+    hour =  Math.floor(time / 3600);
+    min =  Math.floor((time / 60) % 60);
+    sec =  Math.floor(time % 60);
+
+    let hours = (hour >= 10) ? hour : "0" + hour;
+    let mins = (min >= 10) ? min : "0" + min;
+    let secs = (sec >= 10) ? sec : "0" + sec;
+    $('#time_output').text(hours + ":" + mins + ":" + secs);
 }
